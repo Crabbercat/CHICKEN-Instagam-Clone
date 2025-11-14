6420f7412abb1e22ef520e0eb20a67ff810aadc4
Crabbercat <102230292@sv1.dut.udn.vn>
2025-11-15 02:00:34 +0700

    refactor: Reorder import statements for better readability in TmpPost component

--- app/tmp/post.tsx
+++ app/tmp/post.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image, ScrollView, Pressable } from 'react-native';
 import { useLocalSearchParams, useRouter } from 'expo-router';
+import React from 'react';
+import { Image, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function TmpPost(): React.ReactElement {
   const params = useLocalSearchParams();
